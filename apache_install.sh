b7014822b98c725066e20e8804a1391fce876048
Hoangdinh85 <hdinh47056@gmail.com>
2018-09-27 20:04:56 -0700

    Update apache_install.sh

--- apache_install.sh
+++ apache_install.sh
@@ -1,8 +1,7 @@
 [hdinh47056@apache ~]$ sudo su (become root)
-yum install httpd
-systemctl enable httpd
-systemctl enable status httpd
-systemctl status httpd
-systemctl start httpd
+yum install httpd  (To install httpd) 
+systemctl enable httpd (Enable httpd)
+systemctl status httpd (check status)
+systemctl start httpd  (start httpd)
 yum -y install mod_ssl
 systemctl restart httpd
